d99f378a9b663b84ee2694fec7f1dfc011c6afe6
XuXiangJun <xuxiangjun@live.cn>
2026-01-30 16:08:04 +0800

    add DH 3072

--- lib-blufi/src/main/java/blufi/espressif/BlufiClientImpl.java
+++ lib-blufi/src/main/java/blufi/espressif/BlufiClientImpl.java
@@ -68,6 +68,22 @@ class BlufiClientImpl implements BlufiParameter {
             "9172FE9CE98583FF8E4F1232EEF28183C3FE3B1B4C6FAD73" +
             "3BB5FCBC2EC22005C58EF1837D1683B2C6F34A26C1B2EFFA" +
             "886B423861285C97FFFFFFFFFFFFFFFF";
+    private static final String DH_P_3072 = "FFFFFFFFFFFFFFFFADF85458A2BB4A9AAFDC5620273D3CF1" +
+            "D8B9C583CE2D3695A9E13641146433FBCC939DCE249B3EF9" +
+            "7D2FE363630C75D8F681B202AEC4617AD3DF1ED5D5FD6561" +
+            "2433F51F5F066ED0856365553DED1AF3B557135E7F57C935" +
+            "984F0C70E0E68B77E2A689DAF3EFE8721DF158A136ADE735" +
+            "30ACCA4F483A797ABC0AB182B324FB61D108A94BB2C8E3FB" +
+            "B96ADAB760D7F4681D4F42A3DE394DF4AE56EDE76372BB19" +
+            "0B07A7C8EE0A6D709E02FCE1CDF7E2ECC03404CD28342F61" +
+            "9172FE9CE98583FF8E4F1232EEF28183C3FE3B1B4C6FAD73" +
+            "3BB5FCBC2EC22005C58EF1837D1683B2C6F34A26C1B2EFFA" +
+            "886B4238611FCFDCDE355B3B6519035BBC34F4DEF99C0238" +
+            "61B46FC9D6E6C9077AD91D2691F7F7EE598CB0FAC186D91C" +
+            "AEFE130985139270B4130C93BC437944F4FD4452E2D74DD3" +
+            "64F2E21E71F54BFF5CAE82AB9C9DF69EE86D2BC522363A0D" +
+            "ABC521979B0DEADA1DBF9A42D5C4484E0ABCD06BFA53DDEF" +
+            "3C1B20EE3FD59D7C25E41D2B66C62E37FFFFFFFFFFFFFFFF";
     private static final String DH_G = "2";
     private static final String AES_TRANSFORMATION = "AES/CFB/NoPadding";
     private static final int SECURITY_V1 = 1;
@@ -810,12 +826,10 @@ class BlufiClientImpl implements BlufiParameter {
         final BigInteger dhG;
         int securityVersion = getSecurityVersion();
         if (securityVersion == SECURITY_V2) {
-            Log.d(TAG, "postNegotiateSecurity: 2048");
-            dhLength = 2048;
-            dhP = new BigInteger(DH_P_2048, radix);
+            dhLength = 3072;
+            dhP = new BigInteger(DH_P_3072, radix);
             dhG = new BigInteger(DH_G);
         } else {
-            Log.d(TAG, "postNegotiateSecurity: 1024");
             dhLength = 1024;
             dhP = new BigInteger(DH_P, radix);
             dhG = new BigInteger(DH_G);
